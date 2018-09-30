567dce223a24856b2c61ca7c5c3d52f25490a1f1
Ed Sweeney <ed@onextent.com>
2018-09-30 06:46:24 -0700

    test

--- src/tempmon.rs
+++ src/tempmon.rs
@@ -26,20 +26,20 @@ fn node_name() -> String {
 }
 
 fn temp() -> i32 {
-    let adc_a0: mraa_aio_context = unsafe { mraa_aio_init(0) };
-    if adc_a0.is_null() {
-        panic!("Failed to initialise aio context.");
-    }
-
-    //loop {
-    let adc_value: u32 = unsafe { mraa_aio_read(adc_a0) };
-    let adc_value_float: f32 = unsafe { mraa_aio_read_float(adc_a0) };
-
-    println!("ADC A0 read {}", adc_value);
-    println!("ADC A0 read float - {:.5}", adc_value_float);
-    //}
-
-    unsafe { mraa_aio_close(adc_a0) };
+    // let adc_a0: mraa_aio_context = unsafe { mraa_aio_init(0) };
+    // if adc_a0.is_null() {
+    //     panic!("Failed to initialise aio context.");
+    // }
+    //
+    // //loop {
+    // let adc_value: u32 = unsafe { mraa_aio_read(adc_a0) };
+    // let adc_value_float: f32 = unsafe { mraa_aio_read_float(adc_a0) };
+    //
+    // println!("ADC A0 read {}", adc_value);
+    // println!("ADC A0 read float - {:.5}", adc_value_float);
+    // //}
+    //
+    // unsafe { mraa_aio_close(adc_a0) };
 
     40
 }
